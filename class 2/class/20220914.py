e2f381c9a0c4a9c4077bbd51f94b1cb74324016a
Adrian18881 <Adrianhuang1223@gmail.com>
2022-09-14 19:12:46 +0800

    提示文字

--- class 2/class/20220914.py
+++ class 2/class/20220914.py
@@ -1,1 +1,12 @@
-print("adrian is the best")
+#max()
+#len()
+#字串'sssuhsu'、'duchudhuch'
+#字串可以+、*
+#True、False
+#整數、小數(浮點數)
+#None
+#數字運算+、-、*、/、//、%、**
+#a=1 把右邊存到左邊
+#git add .
+#git commit -m "讚讚喔"
+#git push

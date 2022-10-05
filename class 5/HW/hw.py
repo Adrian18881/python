dc814bf8dc51252c34060b0de94375cb2d153d21
Adrian18881 <Adrianhuang1223@gmail.com>
2022-10-05 19:00:58 +0800

    class 5 hw 1

--- class 5/HW/hw.py
+++ class 5/HW/hw.py
@@ -2,4 +2,9 @@ h = float(input("請輸入身高"))
 w = float(input("請輸入體重"))
 bmi = w / h**2
 print("你的BMI為" + str(bmi))
-bmi = int()
+if bmi >= 14.8 and bmi <= 20.7:
+    print("bmi值正常")
+elif bmi >= 20.7:
+    print("bmi值重")
+elif bmi <= 14.8:
+    print("bmi值輕")

af25ec907b0b47093d77c471218ca46b051ff42c
Adrian18881 <Adrianhuang1223@gmail.com>
2022-10-26 19:33:50 +0800

    class 7 hw

--- class 7/HW/hw 1.py
+++ class 7/HW/hw 1.py
@@ -1,2 +1,5 @@
-input("輸入箭頭大小")
-for i in range()
+x = int(input("輸入箭頭大小"))
+for i in range(1, x + 1):
+    print(" " * (x - i) + "*" * (i * 2 - 1))
+for i in range(x):
+    print(" " * (x - 1) + "*")

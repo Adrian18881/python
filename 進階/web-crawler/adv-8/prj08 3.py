f6dbdf4ac48ce3d3699ed914cee10b2481553c21
Adrian18881 <Adrianhuang1223@gmail.com>
2023-05-03 18:46:10 +0800

    prj08

---
+++ 進階/web-crawler/adv-8/prj08 3.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import requests
+import os
+import sys
+import datetime
+
+os.chdir(sys.path[0])
+
+api_key = "892da2f13edf3c7f382637760e72d224"
+
+base_url = "https://api.openweathermap.org/data/2.5/onecall?"
+lon = "121.5319"
+lat = "25.0478"
+exclude = "minutely,hourly"
+units = "metric"
+lang = "zh_tw"
+
+send_url = base_url
+send_url += "lat=" + lat
+send_url += "&lon=" + lon
+send_url += "&exclude=" + exclude
+send_url += "&appid=" + api_key
+send_url += "&units=" + units
+send_url += "&lang=" + lang
+
+print(send_url)
+
+response = requests.get(send_url)
+info = response.json()
+
+X_date = []
+Y_temp = []
+if "daily" in info.keys():
+    for i in range(7):
+        temp = info["daily"][i]["temp"]["day"]
+        time = datetime.datetime.fromtimestamp(
+            info["daily"][i]["dt"]).strftime("%m/%d")
+        print(f"{time}的溫度是{temp}度")
+        X_date.append(time)
+        Y_temp.append(temp)
+
+else:
+    print("Request Fail")
+
+fig, ax = plt.subplots()
+ax.plot(X_date, Y_temp)
+ax.set_xlabel('X label')
+ax.set_ylabel('Y label')
+ax.set_title('My title')
+
+plt.show()
